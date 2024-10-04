c2c1ab78aa6756348520decce5b6d62801a40fac
alihassantahir <104299987+alihassantahir@users.noreply.github.com>
2024-10-05 00:57:54 +0500

    Add files via upload

---
+++ background.js
@@ -0,0 +1,246 @@
+const APPNAME = 'Corrlinks Extension 1';
+const SERVER = 'https://theeblvd.ngrok.io';
+console.debug('background:', `${APPNAME} background.js started`);
+console.debug(`Current ngrok server address: ${SERVER}`);
+console.log('To see all logs, set the console to VERBOSE');
+
+const C = {
+  WEBSITE_DETAILS: {
+    TITLE: 'CorrLinks',
+    HOST: 'www.corrlinks.com',
+    LOGIN: 'www.corrlinks.com/en-US/login',
+    INBOX: 'https://www.corrlinks.com/en-US/mailbox/inbox'
+  },
+  SERVER: {
+    SERVER: SERVER,
+    SEND_POST_MESSAGE: `${SERVER}/message-from-corrlinks`
+  }
+};
+const onIcon = {
+  path: './images/wc-on.png'
+};
+const offIcon = {
+  path: './images/wc-off.png'
+};
+
+let STATE = {
+  running: false,
+  tab: null
+};
+
+chrome.runtime.onInstalled.addListener(function(details) {
+  const fn = 'chrome.runtime.onInstalled.addListener:';
+  let msg = details.reason === 'install' ? `${APPNAME} Installed` : `${APPNAME} update Installed`;
+  console.log(fn, msg);
+});
+
+chrome.action.onClicked.addListener(function() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    if (!STATE.running) {
+      start();
+    } else {
+      reloadTab(STATE.tab?.id);
+      stop();
+
+    }
+  });
+});
+
+function start() {
+  const fn = 'start:';
+  if (STATE.running) return false;
+
+  console.debug(fn);
+  chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+    const tab = tabs[0];
+    STATE.tab = tab;
+
+    chrome.tabs.update(tab.id, { url: C.WEBSITE_DETAILS.INBOX }, function() {
+      chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
+
+        if (tabId === tab.id && changeInfo.status === 'complete') {
+          chrome.tabs.onUpdated.removeListener(listener);
+          const result = isValidSite(tab);
+
+          if (!result.isValid) {
+            handleInvalidSite(tab, result.msg);
+            return;
+          }
+
+          console.debug(fn, 'completed. Handing off to serverMessageListener');
+
+          chrome.action.setIcon(onIcon);
+          STATE.running = true;
+
+          const msg = {
+            message: "START_INTEGRATION"
+          };
+          sendMessageToTab(tab.id, msg);
+          setupMessageListeners();
+        }
+      });
+    });
+  });
+}
+
+function stop() {
+  const fn = 'stop:';
+  console.debug(fn, 'stopping integration');
+  chrome.action.setIcon(offIcon);
+
+  const msg = { message: "STOP_INTEGRATION" };
+  if (STATE.tab) sendMessageToTab(STATE.tab.id, msg);
+
+  resetState();
+}
+
+
+
+function reloadTab(tabId, tabURL) {
+  if (!tabId) return;
+
+  if (tabURL) {
+
+    chrome.tabs.update(tabId, { url: tabURL }, () => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError.message);
+      }
+    });
+  } 
+
+else {
+    chrome.tabs.reload(tabId, () => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError.message);
+      }
+    });
+  }
+}
+
+
+function handleInvalidSite(tab, msg) {
+  resetState();
+  showAlert(tab.id, tab.url, msg);
+}
+
+function isValidSite(tab) {
+
+  if (tab.url.includes(C.WEBSITE_DETAILS.LOGIN)) {
+    console.debug('Invalid host:', tab.url);
+    return { isValid: false, msg: 'Login Page Detected: Please login and retry.' }; 
+  }
+
+  if (!tab.title.includes(C.WEBSITE_DETAILS.TITLE)) {
+    console.debug('Invalid title:', tab.title);
+    return { isValid: false, msg: `This extension can only be activated on a site with the title ${C.WEBSITE_DETAILS.TITLE}`}; 
+  }
+  
+  if (!tab.url.includes(C.WEBSITE_DETAILS.HOST)) {
+    console.debug('Invalid host:', tab.url);
+    
+    return { 
+      isValid: false, 
+      msg: `This extension can only be activated on a site hosted on ${C.WEBSITE_DETAILS.HOST}` 
+    };
+  }
+
+  return { isValid: true, msg: 'Site is valid.' };
+}
+
+
+
+function showAlert(tabID, tabURL, message) {
+  if (
+    tabURL?.startsWith("chrome://") || // Chrome internal URLs
+    tabURL?.startsWith("about:") || // About pages
+    tabURL?.startsWith("file://") || // Local file access
+    tabURL?.startsWith("data:") || // Data URLs
+    tabURL?.includes("://localhost") || // Localhost access
+    tabURL?.includes("127.0.0.1") || // Local IP access
+    tabURL?.startsWith("chrome-extension://") || 
+    tabURL?.startsWith("view-source:") // View source 
+  )
+
+ return // Should return because can't show alerts on these page and abort
+
+  chrome.scripting.executeScript({
+    target: { tabId: tabID },
+    func: (alertMessage) => alert(alertMessage),
+    args: [message],
+  });
+}
+
+function resetState() {
+  STATE.running = false;
+  STATE.tab = null;
+  chrome.action.setIcon(offIcon);
+  removeMessageListeners();
+}
+
+function sendMessageToTab(tabId, message) {
+  chrome.tabs.sendMessage(tabId, message)
+    .then(response => {})
+    .catch(error => {});
+}
+
+function sendMessageToServer(data) {
+  const fn = 'sendMessageToServer:';
+  const url = C.SERVER.SEND_POST_MESSAGE;
+console.log("Sending request to:", url);
+
+  const config = {
+    method: 'POST',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  };
+
+  fetch(url, config)
+    .then(r => {
+      if (!r.ok) return Promise.reject(`HTTP error: ${r.status}`);
+      if (!r.headers.get("content-type")?.includes("application/json")) return Promise.reject('Non-JSON response');
+      return r.json();
+    })
+    .then(responseData => {
+      console.debug(fn, 'Response data:', responseData);
+    })
+    .catch(error => {
+      console.debug('Error:', error);
+    });
+}
+
+function setupMessageListeners() {
+  console.debug('setupMessageListeners:');
+  chrome.runtime.onMessage.addListener(chrome_runtime_onMessage_listener);
+}
+
+function removeMessageListeners() {
+  console.debug('removeMessageListeners:');
+  chrome.runtime.onMessage.removeListener(chrome_runtime_onMessage_listener);
+}
+
+function chrome_runtime_onMessage_listener(request, sender, sendResponse) {
+  console.log('chrome_runtime_onMessage_listener:', { request });
+  if (request.message === "QUEUE_NEW_MESSAGE_TO_WHATS_APP") {
+    sendMessageToServer(request.data);
+  }
+  sendResponse({ status: "Received Msg" });
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'getState') {
+    sendResponse({ state: STATE.running });
+  }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'setState') {
+   	stop()
+  }
+});
+
+chrome.tabs.onRemoved.addListener((tabId) => {
+  if (STATE.tab && STATE.tab.id === tabId) {
+    resetState();
+    console.debug('Tab closed, reset state.');
+  }
+});
---
+++ content.js
@@ -0,0 +1,640 @@
+const APPNAME = 'Corrlinks Extension 1';
+const REFRESH_INTERVAL = 10 // This is the time in minutes Extension refreshes the page...
+
+let C = {
+  MESSAGES: {
+    START_INTEGRATION: 'START_INTEGRATION',
+  }
+};
+
+const STATE = {
+  inboxRefreshInterval: null,
+  messageRefreshInterval: null,
+  inboxMonitorCycles: 0,
+  stopNow: false,
+};
+
+function requestState() {
+
+  chrome.runtime.sendMessage({
+    action: 'getState'
+  }, (response) => {
+    if (response && response.state) {
+      const currentState = response.state;
+
+      if (currentState) {
+
+        startUp();
+
+      }
+    }
+  });
+}
+
+function isLoggedOut() { // This fn sends msg to BG script to ABORT if user is logged out... 
+const loginElement = document.querySelector('img[alt="Logged Out"]'); 
+
+  if (loginElement) {
+    console.debug('Login element exists. Sending STOP message.');
+    	setState();
+      STATE.stopNow = true;
+      
+  }
+}
+
+function setState() {
+  chrome.runtime.sendMessage({ action: 'setState' });
+}
+
+
+requestState()
+
+function isDefaultPage() {
+  const targetUrls = [
+    'https://www.corrlinks.com/en-US/',
+    'https://www.corrlinks.com/es-US/'
+  ];
+
+  return targetUrls.some(url => window.location.href === url);
+}
+
+function isLoginPage() {
+  const targetUrls = [
+    'https://www.corrlinks.com/en-US/login',
+    'https://www.corrlinks.com/es-US/login'
+  ];
+
+  return targetUrls.some(url => window.location.href === url);
+}
+
+const utils = {
+  page: {
+    readMessage: {
+      retrieveEmailAddress: () => {
+        const fn = 'retrieveEmailAddress:';
+        const multiselectElement = document.querySelector('ejs-multiselect[data-initial-value]');
+        const result = multiselectElement ? multiselectElement.getAttribute('data-initial-value') : null;
+        console.debug(fn, ` result=${result}`);
+        return result;
+      },
+      retrieveSubjectLine: () => {
+        const fn = 'retrieveSubjectLine:';
+
+        const found = document.querySelector('input[formcontrolname="subject"]');
+
+        const result = found ? found.value : false;
+
+        console.debug(fn, ` result=${result}`);
+        return result;
+      },
+      retrieveAccountAddress: async () => {
+        const fn = 'retrieveAccountAddress:';
+        const userButton = document.querySelector('header button:has(div.user-initials)');
+        userButton.click();
+
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            const listItem = document.getElementById('loggedInUser');
+
+            if (listItem) {
+              resolve(listItem.innerText);
+            } else {
+              reject(new Error('List item not found'));
+            }
+          }, 500);
+        });
+      },
+
+      retrieveFirstLine: () => {
+        const fn = 'retrieveFirstLine:';
+        const found = document.querySelector('textarea[formcontrolname="message"]');
+        const result = found ? found.value : false;
+        console.debug(fn, ` result=${result}`);
+        return result;
+      },
+      retrieveTextarea: () => {
+        const fn = 'retrieveTextarea:';
+        const found = document.querySelector('textarea[formcontrolname="message"]');
+        const result = found ? found.value : false;
+        console.debug(fn, ` result=${result}`);
+        return result;
+      },
+      closeReadMessage: () => {
+        const fn = 'closeReadMessage:';
+        console.debug(fn, 'called');
+        setTimeout(() => {
+          const cancelButton = [...document.querySelectorAll('button.e-btn')].filter(
+            btn => btn.textContent.trim() === 'Close'
+          )[0];
+
+          if (cancelButton) {
+
+            simulateClick(cancelButton)
+            console.debug(fn, 'Button clicked');
+          }
+        }, 1000);
+      },
+
+      retrieveMessageFromUI: async () => {
+
+        const fn = 'retrieveMessageFromUI:';
+        console.debug(fn);
+
+        const emailAddress = utils.page.readMessage.retrieveEmailAddress();
+
+        const messageText = utils.page.readMessage.retrieveTextarea();
+        const message = messageText;
+
+        const subject = utils.page.readMessage.retrieveSubjectLine();
+        const mobileNumber = formatPhoneNumber(subject) || "NA";
+
+        const accountAddress = await utils.page.readMessage.retrieveAccountAddress();
+
+        const data = {
+          mobileNumber,
+          message,
+          emailAddress,
+          subject,
+          accountAddress
+        };
+
+        console.debug(fn, 'data collected', data);
+        return data;
+
+      },
+      isDisplayingMessage: () => {
+        const fn = 'isDisplayingMessage:';
+        const result = document.title.includes('Inbox Message') && window.location.href.includes("https://www.corrlinks.com/en-US/mailbox/inbox/message/");
+        console.debug(fn, `result=${result}`);
+        return result;
+      },
+
+    },
+    inbox: {
+      isDisplayingInbox: () => {
+        const fn = 'isDisplayingInbox:';
+
+        const urlMatches = location.href.includes('www.corrlinks.com/en-US/mailbox/inbox');
+
+        const result = urlMatches;
+
+        console.debug(fn, `result=${result}`);
+        return result;
+      },
+      getUnreadMessagesCheckbox: () => {
+        const fn = 'getUnreadMessagesCheckbox:';
+        let unreadMessagesCheckbox = document.querySelector('input[type="checkbox"][formcontrolname="unreadMessagesOnly"]');
+        console.debug(fn, `result=`, {
+          unreadMessagesCheckbox
+        });
+        return unreadMessagesCheckbox;
+      },
+      setDisplayToUnreadMessagesOnly: () => {
+        const fn = 'setDisplayToUnreadMessagesOnly:';
+        console.debug(fn);
+
+        let unreadMessagesCheckbox = utils.page.inbox.getUnreadMessagesCheckbox();
+        if (!unreadMessagesCheckbox) return
+        if (!unreadMessagesCheckbox.checked) {
+          console.debug(fn, 'setting UI to only display unread messages');
+          unreadMessagesCheckbox.click();
+        } else {
+          console.debug(fn, 'UI already set to only display unread messages');
+        }
+      },
+      refresh: () => {
+        const fn = 'refresh:';
+        console.debug(fn, 'called');
+
+        const unreadMessagesCheckbox = utils.page.inbox.getUnreadMessagesCheckbox();
+        if (!unreadMessagesCheckbox && (!isLoginPage() || isDefaultPage())) {
+          window.location.href = "https://www.corrlinks.com/en-US/mailbox/inbox"
+
+        }
+
+        if (unreadMessagesCheckbox) unreadMessagesCheckbox.click();
+
+      },
+      getClickableItem_selectedMessage: () => {
+        const fn = 'getClickableItem_selectedMessage:';
+
+        let result = document.querySelector('tr[aria-selected="true"]');
+
+        result = result?.closest('tr')?.querySelector('td');
+
+        console.debug(fn, `result=${result}`);
+        return result;
+      },
+      getClickableItem_unreadMessage: () => {
+        const fn = 'getClickableItem_unreadMessage:';
+        const result = document.querySelector('tr.e-row.grid-row-bold') || null;
+        console.debug(fn, `result=${result}`);
+        return result;
+      },
+      openMessageCollectSendAndClose: async (clickableItem) => {
+
+        const fn = 'openMessageCollectSendAndClose:';
+        console.debug(fn, {
+          clickableItem
+        });
+
+        if (!clickableItem) {
+          console.debug(fn, 'clickableItem is null. Abort');
+          return Promise.resolve(null);
+        }
+
+        const checkbox = clickableItem.closest('tr').querySelector('td[data-colindex="1"]');
+        if (checkbox) {
+          checkbox.click();
+        }
+
+        clickableItem.click();
+        const data = await utils.general.pageChangedtoMsg();
+
+        if (utils.general.isValidDataset(data)) {
+          const msg = {
+            data: {
+              to: data.mobileNumber,
+              from: data.emailAddress,
+              body: data.message,
+              subject: data.subject,
+              account: data.accountAddress
+            },
+            message: "QUEUE_NEW_MESSAGE_TO_WHATS_APP"
+          };
+
+
+          chrome.runtime.sendMessage(null, msg);
+        }
+
+        utils.page.readMessage.closeReadMessage();
+
+      },
+      hasNewMessage: () => {
+        const fn = 'hasNewMessage:';
+        const result = !!window.document.querySelectorAll('td.BoldItem').length;
+        console.debug(fn, `result=${result}`);
+        return result;
+      },
+    }
+  },
+  general: {
+    isValidDataset: (potentialDataToSend) => {
+      const fn = 'isValidDataset:';
+      const result = potentialDataToSend.mobileNumber && potentialDataToSend.message && potentialDataToSend.emailAddress && potentialDataToSend.subject && potentialDataToSend.accountAddress;
+      console.debug(fn, ` result=${result}`);
+      return result;
+    },
+
+    pageChangedtoMsg: async () => {
+      const fn = 'pageChangedtoMsg:';
+      const timeoutDuration = 5000;
+      const checkInterval = 1000;
+
+      return new Promise(async (resolve) => {
+        const startTime = Date.now();
+
+        const intervalId =  setInterval( async () => {
+
+          if (STATE.stopNow) {
+            clearInterval(STATE.intervalId);
+            STATE.intervalId = null;
+            console.log(fn, 'stopNow');
+            return;
+
+          }
+
+          const isDisplayingMessage = utils.page.readMessage.isDisplayingMessage();
+          console.debug(fn, `Checking message display: ${isDisplayingMessage}`);
+
+          if (isDisplayingMessage) {
+
+            const retrievedData = await utils.page.readMessage.retrieveMessageFromUI();
+            clearInterval(intervalId);
+
+            resolve(retrievedData);
+
+          }
+          if (Date.now() - startTime >= timeoutDuration) {
+            clearInterval(intervalId);
+            resolve(null);
+          }
+        }, checkInterval);
+      });
+    }
+
+  }
+}
+
+function startInboxMonitor() {
+  const fn = 'startInboxMonitor:';
+  console.debug(fn);
+  console.debug(fn, 'IMPORTANT: uncomment utils.page.inbox.setDisplayToUnreadMessagesOnly()');
+  utils.page.inbox.setDisplayToUnreadMessagesOnly();
+  startIntervalForInboxMonitor();
+}
+
+async function startIntervalForInboxMonitor() {
+
+  let paused = false;
+  let pausedAnnounced = false;
+  const fn = 'startIntervalForInboxMonitor:';
+  STATE.inboxRefreshInterval = window.setInterval(async () => {
+  isLoggedOut()
+    console.debug(fn, `interval cycle: ${++STATE.inboxMonitorCycles}`);
+    if (STATE.stopNow) {
+      paused = true;
+      pausedAnnounced = true;
+      window.clearInterval(STATE.inboxRefreshInterval);
+      STATE.inboxRefreshInterval = null;
+      console.log(fn, 'stopNow');
+      return;
+    }
+    if (paused) {
+      if (!pausedAnnounced) {
+        console.debug(fn, 'Interval PAUSED');
+        pausedAnnounced = true;
+      }
+      return;
+    }
+
+    let clickableMessageElement = utils.page.inbox.getClickableItem_selectedMessage();
+    if (!clickableMessageElement) {
+      clickableMessageElement = utils.page.inbox.getClickableItem_unreadMessage();
+    }
+
+    if (!clickableMessageElement) {
+      return utils.page.inbox.refresh();
+    }
+
+    paused = true;
+
+    await utils.page.inbox.openMessageCollectSendAndClose(clickableMessageElement);
+    paused = false;
+    pausedAnnounced = false;
+    console.debug(fn, 'Interval UNPAUSED');
+
+  }, REFRESH_INTERVAL * 1000);
+}
+
+function startUp() {
+  const fn = 'startUp:';
+  console.debug(fn);
+
+  STATE.stopNow = false;
+
+  startInboxMonitor();
+
+  console.debug(fn, 'complete');
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    const fn = 'content.js: chrome.runtime.onMessage.addListener:';
+    console.debug(fn, {
+      request,
+      sender,
+      sendResponse
+    });
+    if (request.message === "START_INTEGRATION") {
+      console.log('Start Integration message received');
+      startUp();
+    }
+    if (request.message === "STOP_INTEGRATION") {
+      console.log('Stop Integration message received');
+      STATE.stopNow = true;
+      console.debug(fn, 'stopNow set to true');
+      return;
+    }
+
+  }
+);
+
+const countries = [{
+    code: 'US',
+    label: 'United States',
+    phone: '1',
+    suggested: true,
+    phoneLength: 10,
+  }
+
+];
+
+countries.sort(
+  (a, b) =>
+  b.phone.replace(/\D/g, '').length - a.phone.replace(/\D/g, '').length
+);
+
+function isValidUSPhoneNumber(number) {
+
+  if (checkInternationalPhone(number)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+var validWorldPhoneChars = "+";
+var minDigitsInIPhoneNumber = 10;
+var maxDigitsInIPhoneNumber = 13;
+var AreaCode = new Array(205, 251, 659, 256, 334, 907, 403, 780, 264, 268, 520, 928, 480, 602, 623, 501, 479, 870, 242, 246, 441, 250, 604, 778, 284, 341, 442, 628, 657, 669, 747, 752, 764, 951, 209, 559, 408, 831, 510, 213, 310, 424, 323, 562, 707, 369, 627, 530, 714, 949, 626, 909, 916, 760, 619, 858, 935, 818, 415, 925, 661, 805, 650, 600, 809, 345, 670, 211, 720, 970, 303, 719, 203, 475, 860, 959, 302, 411, 202, 767, 911, 239, 386, 689, 754, 941, 954, 561, 407, 727, 352, 904, 850, 786, 863, 305, 321, 813, 470, 478, 770, 678, 404, 706, 912, 229, 710, 473, 671, 808, 208, 312, 773, 630, 847, 708, 815, 224, 331, 464, 872, 217, 618, 309, 260, 317, 219, 765, 812, 563, 641, 515, 319, 712, 876, 620, 785, 913, 316, 270, 859, 606, 502, 225, 337, 985, 504, 318, 318, 204, 227, 240, 443, 667, 410, 301, 339, 351, 774, 781, 857, 978, 508, 617, 413, 231, 269, 989, 734, 517, 313, 810, 248, 278, 586, 679, 947, 906, 616, 320, 612, 763, 952, 218, 507, 651, 228, 601, 557, 573, 636, 660, 975, 314, 816, 417, 664, 406, 402, 308, 775, 702, 506, 603, 551, 848, 862, 732, 908, 201, 973, 609, 856, 505, 575, 585, 845, 917, 516, 212, 646, 315, 518, 347, 718, 607, 914, 631, 716, 709, 252, 336, 828, 910, 980, 984, 919, 704, 701, 283, 380, 567, 216, 614, 937, 330, 234, 440, 419, 740, 513, 580, 918, 405, 905, 289, 647, 705, 807, 613, 519, 416, 503, 541, 971, 445, 610, 835, 878, 484, 717, 570, 412, 215, 267, 814, 724, 902, 787, 939, 438, 450, 819, 418, 514, 401, 306, 803, 843, 864, 605, 869, 758, 784, 731, 865, 931, 423, 615, 901, 325, 361, 430, 432, 469, 682, 737, 979, 214, 972, 254, 940, 713, 281, 832, 956, 817, 806, 903, 210, 830, 409, 936, 512, 915, 868, 649, 340, 385, 435, 801, 802, 276, 434, 540, 571, 757, 703, 804, 509, 206, 425, 253, 360, 564, 304, 262, 920, 414, 715, 608, 307, 867)
+
+function isInteger(s)
+
+{
+  var i;
+
+  for (i = 0; i < s.length; i++) {
+    var c = s.charAt(i);
+    if (((c < "0") || (c > "9"))) return false;
+
+  }
+  return true;
+}
+
+function stripCharsInBag(s, bag)
+
+{
+  var i;
+
+  var returnString = "";
+  for (i = 0; i < s.length; i++)
+
+  {
+    var c = s.charAt(i);
+    if (bag.indexOf(c) == -1) returnString += c;
+  }
+  return returnString;
+
+}
+
+function checkInternationalPhone(strPhone) {
+
+  if (strPhone.indexOf("00") == 0) strPhone = strPhone.substring(2)
+
+  if (strPhone.indexOf("+") > 1) return false
+
+  if (strPhone.indexOf("+") == 0) strPhone = strPhone.substring(1)
+
+  if (strPhone.indexOf("(") == -1 && strPhone.indexOf(")") != -1) return false
+
+  if (strPhone.indexOf("(") != -1 && strPhone.indexOf(")") == -1) return false
+
+  let s = stripCharsInBag(strPhone, validWorldPhoneChars);
+
+  if (strPhone.length > 10) {
+    var CCode = s.substring(0, s.length - 10);
+  } else {
+    CCode = "";
+  }
+
+  if (strPhone.length > 7) {
+    var NPA = s.substring(s.length - 10, s.length - 7);
+  } else {
+    NPA = ""
+  }
+
+  var NEC = s.substring(s.length - 7, s.length - 4)
+
+  if (CCode != "" && CCode != null) {
+
+    if (CCode != "1" && CCode != "011" && CCode != "001") return false
+
+  }
+
+  if (NPA != "") {
+
+    if (checkAreaCode(NPA) == false) {
+
+      return false
+
+    }
+
+  } else {
+    return false
+  }
+
+  return (isInteger(s) && s.length >= minDigitsInIPhoneNumber && s.length <= maxDigitsInIPhoneNumber);
+
+}
+
+function checkAreaCode(val) {
+
+  var res = false;
+
+  for (var i = 0; i < AreaCode.length; i++) {
+
+    if (AreaCode[i] == val) res = true;
+
+  }
+
+  return res
+
+}
+
+function stripAreaCode(input) {
+  if (input.length === 14 && input.charAt(3) === '1') {
+    return input.slice(3);
+
+  }
+  return input;
+}
+
+function formatPhoneNumber(localNumber) {
+  if (!localNumber) return
+  let cleanedLocalNumber = localNumber.replace(/\D/g, '');
+
+  if (cleanedLocalNumber.length === 14) {
+    cleanedLocalNumber = stripAreaCode(cleanedLocalNumber);
+  }
+
+  const country = countries.find((c) => {
+    const cleanedCountryPhone = c.phone.replace(/\D/g, '');
+    return cleanedLocalNumber.startsWith(cleanedCountryPhone);
+  });
+
+  if (isValidUSPhoneNumber(cleanedLocalNumber) || !country) {
+    let formattedNumber;
+    if (cleanedLocalNumber.length === 10) {
+      formattedNumber = `(${cleanedLocalNumber.slice(0, 3)}) ${cleanedLocalNumber.slice(3, 6)}-${cleanedLocalNumber.slice(6)}`;
+      return formattedNumber;
+    } else if (cleanedLocalNumber.length === 11 && cleanedLocalNumber[0] === '1') {
+      formattedNumber = `(${cleanedLocalNumber.slice(1, 4)}) ${cleanedLocalNumber.slice(4, 7)}-${cleanedLocalNumber.slice(7)}`;
+      return formattedNumber;
+    } else if (cleanedLocalNumber.length === 11 && isValidUSPhoneNumber(cleanedLocalNumber)) {
+      formattedNumber = `${cleanedLocalNumber.slice(0, 3)} ${cleanedLocalNumber.slice(3, 6)}-${cleanedLocalNumber.slice(6)}`;
+      return formattedNumber;
+    }
+  }
+
+  if (country) {
+    const cleanedCountryCode = country.phone.replace(/\D/g, '');
+    const localNumberOnly = cleanedLocalNumber.slice(cleanedCountryCode.length);
+
+    let isValid = false;
+    if (Array.isArray(country.phoneLength)) {
+      isValid = country.phoneLength.includes(localNumberOnly.length);
+    } else if (typeof country.phoneLength === 'object') {
+      const {
+        min,
+        max
+      } = country.phoneLength;
+      isValid = localNumberOnly.length >= min && localNumberOnly.length <= max;
+    } else if (country.phoneLength) {
+      isValid = localNumberOnly.length === country.phoneLength;
+    } else {
+      isValid = true;
+    }
+
+    if (localNumberOnly.length < 5) {
+      return;
+    }
+    if (!isValid && localNumberOnly.length > 13) {
+      return;
+    }
+
+    let formattedNumber;
+    if (country.label === 'USA' || country.label === 'Canada') {
+      if (cleanedLocalNumber.length === 10) {
+        formattedNumber = `(${cleanedLocalNumber.slice(0, 3)}) ${cleanedLocalNumber.slice(3, 6)}-${cleanedLocalNumber.slice(6)}`;
+      } else {
+        formattedNumber = `${localNumberOnly.slice(0, 3)} ${localNumberOnly.slice(3, 6)}-${localNumberOnly.slice(6)}`;
+      }
+    } else {
+      const fullNumber = `+${cleanedCountryCode}${localNumberOnly}`;
+      formattedNumber = fullNumber;
+    }
+
+    return formattedNumber;
+  }
+}
+
+function simulateClick(element) {
+  if (!element) return;
+
+  const mouseDownEvent = new MouseEvent('mousedown', {
+    bubbles: true,
+    cancelable: true,
+    view: window,
+  });
+
+  const mouseUpEvent = new MouseEvent('mouseup', {
+    bubbles: true,
+    cancelable: true,
+    view: window,
+  });
+
+  const clickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    view: window,
+  });
+
+  element.dispatchEvent(mouseDownEvent);
+  setTimeout(() => {
+    element.dispatchEvent(mouseUpEvent);
+    element.dispatchEvent(clickEvent); 
+    element.focus(); 
+  }, 100); 
+}
+
+
